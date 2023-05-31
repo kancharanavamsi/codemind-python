9b32eecfb1aa803b2271d60517ac27c2476ed638
vamsi bharadwaj <99869566+kancharanavamsi@users.noreply.github.com>
2023-05-31 10:41:35 +0530

    n=int(input()) a=list(map(int,input().split())) c=[] for i in a:     if i not in c:         print(i,'',end='')         print(a.count(i),'',end='')         c.append(i)

--- Element_and_Count.py
+++ Element_and_Count.py
@@ -3,7 +3,6 @@ a=list(map(int,input().split()))
 c=[]
 for i in a:
     if i not in c:
-        print(i,end=" ")
-        print(a.count(i),end=" ")
+        print(i,'',end='')
+        print(a.count(i),'',end='')
         c.append(i)
-        
