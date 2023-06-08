c804d73219f3e4cc3464ddeae83752ceab56d486
vamsi bharadwaj <99869566+kancharanavamsi@users.noreply.github.com>
2023-06-08 11:53:37 +0530

    s=input().lower().split() a=list(str(s[0])) c=0 k=[] for i in a:     c1=0     for j in range(1,len(s)):         if i in s[j]:             c1=c1+1     if(c1==len(s)-1):         k.append(i)         c=1 if(c==0):     print("-1") else:     print("".join(k))

---
+++ Common_characters_-1.py
@@ -0,0 +1,17 @@
+s=input().lower().split()
+a=list(str(s[0]))
+c=0
+k=[]
+for i in a:
+    c1=0
+    for j in range(1,len(s)):
+        if i in s[j]:
+            c1=c1+1
+    if(c1==len(s)-1):
+        k.append(i)
+        c=1
+if(c==0):
+    print("-1")
+else:
+    print("".join(k))
+            
